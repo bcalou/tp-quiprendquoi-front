bbbc1c87e8e0eda9e6dfbb2eeee9918d291aa839
bcalou <bastien.calou@gmail.com>
2020-03-16 00:11:17 +0100

    11 Récupérer les paramètres du POST

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   res.render('index', { title: 'Qui prend quoi ?' });
 });
 
 app.post('/party', (req, res) => {
+  console.log(req.body);
   res.send('Post ok!');
 });
 
