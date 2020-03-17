c588be5943991f0791c79b213b919dda713951eb
bcalou <bastien.calou@gmail.com>
2020-03-17 18:41:02 +0100

    33 Intercepter une requête

--- sw.js
+++ sw.js
@@ -1,3 +1,9 @@
 addEventListener('install', (event) => {
   console.log('Hello from the service worker');
 });
+
+addEventListener('fetch', (event) => {
+  if (event.request.headers.get('Accept').includes('text/html')) {
+    event.respondWith(fetch(event.request));
+  }
+});
