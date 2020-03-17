6aa73e22185c2437615e0cfb2dbd3666aa81e1b5
bcalou <bastien.calou@gmail.com>
2020-03-17 02:22:09 +0100

    28 L'API de partage

---
+++ scripts/share.js
@@ -0,0 +1,17 @@
+if (navigator.share) {
+  document.querySelectorAll('[data-share-url]').forEach(($shareEl) => {
+    const $button = document.createElement('button');
+    $button.innerHTML = 'Partager';
+    $shareEl.parentNode.append($button);
+
+    $button.addEventListener('click', share.bind(this, $shareEl));
+  });
+}
+
+function share($shareEl) {
+  navigator.share({
+    title: $shareEl.getAttribute('data-share-title'),
+    text: $shareEl.getAttribute('data-share-text'),
+    url: $shareEl.getAttribute('data-share-url'),
+  });
+}
