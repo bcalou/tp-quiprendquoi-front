9679f67e801c9ce5ec54e1f29c4faf097120381e
bcalou <bastien.calou@gmail.com>
2020-03-17 21:13:41 +0100

    35 Stocker les pages événement

--- sw.js
+++ sw.js
@@ -9,7 +9,31 @@ addEventListener('install', (event) => {
 addEventListener('fetch', (event) => {
   if (event.request.headers.get('Accept').includes('text/html')) {
     event.respondWith(
-      fetch(event.request).catch(() => caches.match('offline.html'))
+      fetch(event.request)
+        .then((res) => {
+          if (isPartyPage(event.request.url)) {
+            const copy = res.clone();
+            caches
+              .open('parties')
+              .then((cache) => cache.put(event.request, copy));
+            return res;
+          } else {
+            return res;
+          }
+        })
+        .catch(() => {
+          if (isPartyPage(event.request.url)) {
+            return caches
+              .match(event.request)
+              .catch(() => caches.match('offline.html'));
+          } else {
+            return caches.match('offline.html');
+          }
+        }),
     );
   }
 });
+
+function isPartyPage(url) {
+  return /party\/[a-zA-Z0-9]*$/.test(url);
+}
